c12a114f39535e9ddc3058b22b3acf4ad2fb1d06
PosavacIvan <ivanposavac2@gmail.com>
2023-12-06 01:19:37 +0100

    Finished Section 4: Bankey - Login

---
+++ Bankey/Bankey/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Bankey
+//
+//  Created by Ivan Posavac on 01.12.2023..
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchingOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = LoginViewController()
+        
+        return true
+    }
+}
+
